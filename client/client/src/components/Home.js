904d8f546e26809a049ff800f1de9e229d62b379
Dariusz Czeczuk <dariusz.czeczuk@hotmail.com>
2023-04-02 18:55:57 -0400

    FinalProject - dariusz czeczuk - setup

---
+++ client/client/src/components/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return(
+        <div>Beginning...</div>
+    )
+}
+
+export default Home;
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const PORT = 8000
+
+//ENDPOINTS
+
+//catch
+
+//listen on PORT
+
+.listen(PORT,() => {
+    console.info("Listening on port 8000");
+});
