eca15519ea1d3c9b86599f3cd1a6ed062c2fcd7f
Dariusz Czeczuk <dariusz.czeczuk@hotmail.com>
2023-04-08 17:07:42 -0400

    finalProject - dariusz czeczuk - mongoendpoint

--- server/handlers/getAllPlanets.js
+++ server/handlers/getAllPlanets.js
@@ -6,3 +6,25 @@ const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
+
+const getAllPlanets = async(req, res) => {
+    const client = new MongoClient(MONGO_URI, options);
+
+    try{
+        client.connect();
+        const db = client.db("starpath");
+        const data = await db.collection("planets").find({}).toArray();
+        
+    data.length > 0
+    ? res.status(200).send({ status: 200, success: true, data: data })
+    : res
+        .status(404)
+        .send({ status: 404, success: false, data: "Planets not found" });
+} catch (err) {
+  console.log(err);
+} finally {
+  client.close();
+}
+}
+
+module.exports = {getAllPlanets};
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const { getAllPlanets } = require("./handlers/getAllPlanets")
 
 const PORT = 8000;
 
