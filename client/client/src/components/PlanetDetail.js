00a61da7a0cd4989635d0677adaf5a7a3a9aeb30
Dariusz Czeczuk <dariusz.czeczuk@hotmail.com>
2023-04-18 16:02:17 -0400

    FinalProject - dariusz czeczuk - loginRedirect

--- client/client/src/components/PlanetDetail.js
+++ client/client/src/components/PlanetDetail.js
@@ -1,6 +1,11 @@
-
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const PlanetDetail = () => {
+    const {id} = useParams();
+
+    const [state,setState] = useState(null);
 return (
     <>
         <p>He lives</p>
