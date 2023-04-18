1c41e559245cde8065d6df3adde7ac2ea3adbbc3
Dariusz Czeczuk <dariusz.czeczuk@hotmail.com>
2023-04-18 17:50:07 -0400

    FinalProject - dariusz czeczyk - beforeBEWork

--- client/client/src/components/Profile.js
+++ client/client/src/components/Profile.js
@@ -4,6 +4,10 @@ import { useAuth0 } from '@auth0/auth0-react';
 const Profile = () => {
     const { user, isAuthenticated } = useAuth0();
 
+    const doUser = async(req, res) =>{
+        
+    }
+
     return(
         isAuthenticated && (
         <div>
@@ -11,6 +15,7 @@ const Profile = () => {
             <h2>{user.name}</h2>
             <p>{user.email}</p>
             {JSON.stringify(user, null, 2)}
+            <button onClick={doUser} >Confirm User for Launch</button>
         </div>
         )
     )
