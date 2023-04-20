444dc815c76d8eb87edf9f64c58c3734953c1d66
Dariusz Czeczuk <dariusz.czeczuk@hotmail.com>
2023-04-20 19:58:09 -0400

    FinalProject - dariusz czeczuk - Navigation

--- client/client/src/components/QuestSelect.js
+++ client/client/src/components/QuestSelect.js
@@ -1,6 +1,20 @@
 import { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+
+
 
 const QuestSelect = () =>{
+    const { user, isAuthenticated } = useAuth0();
+
+    useEffect(() =>{
+        if(!user){
+            return(<Navigate to="/"/>)
+        }
+        return () =>{
+            //cleanup??
+        }
+    },[])
 
     return(
         <div>
